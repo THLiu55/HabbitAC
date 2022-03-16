31a8825c12cf29d8dc5f400bd1a50e2b710ff96e
liutianhao <tianhao.liu@ucdconnect.ie>
2022-03-16 16:11:42 +0800

    隐藏 action bar

--- app/src/main/java/com/example/habitac/ForgetPwd.java
+++ app/src/main/java/com/example/habitac/ForgetPwd.java
@@ -1,5 +1,6 @@
 package com.example.habitac;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -12,6 +13,11 @@ public class ForgetPwd extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_pwd);
+        // 隐藏 actionbar
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.hide();
+
     }
 
     public static void actionStart(Context context, String data, String data2) {
