6c676740b7ab25afdea0041b4f157e04cc3be9ac
tangyuedi <yuedi.tang@ucdconnect.ie>
2022-03-30 01:57:47 +0800

    Front end implementation of Task

---
+++ app/src/main/java/com/example/habitac/activity/TaskDetails.java
@@ -0,0 +1,14 @@
+package com.example.habitac.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.habitac.R;
+import android.os.Bundle;
+
+public class TaskDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_details);
+    }
+}
