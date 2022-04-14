e0a0cc2ad8feefdf9f4f0acc13412872364f91bc
lxy <xinyue.li2@ucdconnect.ie>
2022-04-14 15:27:18 +0800

    The correct od task database

---
+++ app/src/main/java/com/example/habitac/database/TaskDao.java
@@ -0,0 +1,41 @@
+package com.example.habitac.database;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface TaskDao {
+    @Insert
+    void insertTask(Task ... tasks);
+
+    @Delete
+    void deleteTask(Task ... tasks);
+
+    @Update
+    void updateTask(Task ... tasks);
+
+    @Query("SELECT * FROM Task WHERE frequency & :today != 0 AND isDone = 1")
+    LiveData<List<Task>> getALlDoneTask(int today);
+
+    @Query("SELECT * FROM Task WHERE frequency & :today != 0 AND isDone = 0")
+    LiveData<List<Task>> getALlTodoTask(int today);
+
+    @Insert
+    void insertTaskHistory(TaskHistory ... TaskHistorys);
+
+    @Delete
+    void deleteTaskHistory(TaskHistory ... TaskHistorys);
+
+
+    @Query("SELECT * FROM TaskHistory WHERE date = :theDay AND isDone = 1")
+    LiveData<List<TaskHistory>> getDoneHistoryOf(String theDay);
+
+    @Query("SELECT * FROM TaskHistory WHERE date = :theDay AND isDone = 0")
+    LiveData<List<TaskHistory>> getNotDoneHistoryOf(String theDay);
+}
---
+++ app/src/main/java/com/example/habitac/database/TaskHistory.java
@@ -0,0 +1,44 @@
+package com.example.habitac.database;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(primaryKeys = {"date","id"})
+public class TaskHistory {
+    private String date;
+    private int id;
+    private String name;
+    private int isDone;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getIsDone() {
+        return isDone;
+    }
+
+    public void setIsDone(int isDone) {
+        this.isDone = isDone;
+    }
+}
