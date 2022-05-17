7d498efa6288c3fe34bcc6ca3b19f3ddab689296
hutiansu <tiansu.hu@ucdconnect.ie>
2022-05-17 23:31:15 +0800

    Shop purchase + balance show

--- app/src/main/java/com/example/habitac/fragment/EquipmentFragment.java
+++ app/src/main/java/com/example/habitac/fragment/EquipmentFragment.java
@@ -4,25 +4,33 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.habitac.R;
+import com.example.habitac.activity.Login;
+import com.example.habitac.database.User;
+import com.example.habitac.model.SharedViewModel;
 
 import cn.bmob.v3.*;
 
 public class EquipmentFragment extends Fragment {
+    SharedViewModel sharedViewModel;
+    User loggedUser;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_equipment, container, false);
+
+        sharedViewModel = new ViewModelProvider(Login.login).get(SharedViewModel.class);
+        loggedUser = sharedViewModel.getUser();
 
 
 
-
-        return inflater.inflate(R.layout.fragment_equipment, container, false);
-
-
+        return root;
     }
 }
